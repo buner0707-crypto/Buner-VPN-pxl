0ac14196cad9408d8e2e033d30ccc39d77077f60
buner0707-crypto <buner0707@gmail.com>
2026-08-03 16:38:56 +0500

    Update BunerVpnService.kt

--- app/src/main/java/com/buner/vpn/BunerVpnService.kt
+++ app/src/main/java/com/buner/vpn/BunerVpnService.kt
@@ -20,11 +20,13 @@ class BunerVpnService : VpnService() {
     private var running = false
 
     private val servers = listOf(
-        ServerInfo("Франция-1", "45.32.45.123", 443),
-        ServerInfo("Франция-2", "45.76.89.234", 443),
+            private val servers = listOf(
+        ServerInfo("Франция", "45.32.45.123", 443),
         ServerInfo("Узбекистан", "185.213.155.45", 443),
         ServerInfo("Эстония", "185.165.29.145", 443),
         ServerInfo("Малайзия", "103.27.238.67", 443)
+    )
+
     )
 
     data class ServerInfo(
